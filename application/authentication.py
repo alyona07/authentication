b5c7de21fb196d3c620c75db64c142f26f4fb4b8
alyonajun <alyoniha@gmail.com>
2014-04-03 22:55:20 +0300

    changes of function get_data()

--- application/authentication.py
+++ application/authentication.py
@@ -1,36 +1,37 @@
+__author__ = 'lolo'
+import sys
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-def get_data():
-    if request.method == 'POST':
-        login = str(request.form['login'])
-        password = str(request.form['password'])
-    else:
-        login = str(request.args.get('login'))
-        password = str(request.args.get('password'))
-    login_in = [login, password]
-    return login_in
 
-def file_write(login_in):
+def get_data():
+    result = {}
+    for name in ('login', 'password'):
+        if request.method == 'POST':
+            value = str(request.form[name])
+        else:
+            value = str(request.args.get(name))
+        result[name] = value
+        print(result)
+    return result
+
+def file_write(result):
     with open('login_in.txt', 'w') as f:
-        for item in login_in:
-            f.write(item)
+        for item in result:
+            f.write(result[item])
             f.write('\t')
-    f.close()
 
 def file_open_in(file_txt):
-    f = open(file_txt, 'r')
-    login_in = list(f.readline().split())
-    f.close()
+    with open(file_txt, 'r') as f:
+        login_in = list(f.readline().split())
     return login_in
 
 def file_open_out(file_txt):
     login_out = []
-    f = open(file_txt, 'r')
-    for line in range(0, 3, 1):
-        login_out.append(f.readline().split())
-    f.close()
+    with open(file_txt, 'r') as f:
+        for line in range(0, 3, 1):
+            login_out.append(f.readline().split())
     return login_out
 
 @app.route('/', methods = ['GET', 'POST'])
